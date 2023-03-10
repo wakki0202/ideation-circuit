655251c66cc881392cd57e8099e47a81077a9e2c
wakki0202 <ytk0202@docomo.ne.jp>
2023-03-11 02:27:14 +0900

    change env add

--- libs/client.ts
+++ libs/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-  serviceDomain: "ideation-circuit",
+  serviceDomain: process.env.SERVICE_DOMAIN || "",
   apiKey: process.env.API_KEY || "",
 });
